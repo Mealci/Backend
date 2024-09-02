9c8cddca89b4a0cedf54f442464a1b901978aafa
simlf <lefournsimon@gmail.com>
2024-09-02 16:53:43 +0200

    🎉 Init Springboot project

---
+++ src/main/java/com/mealci/MealciApplication.java
@@ -0,0 +1,13 @@
+package com.mealci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mealci/MealciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mealci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
