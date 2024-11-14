6f714663e6d78839623d2c05e11f7553021b39d3
hugo <hugo.decuq.34@icloud.com>
2024-11-14 14:53:22 +0100

    Create Entity tests

---
+++ src/main/java/com/mealci/tests/core/base/entities/TestEntity.java
@@ -0,0 +1,19 @@
+package com.mealci.tests.core.base.entities;
+
+import com.mealci.core.base.Entity;
+import lombok.Getter;
+
+@Getter
+public final class TestEntity extends Entity<Integer> {
+    public final String test;
+
+    private TestEntity(int id, String test) {
+        super(id);
+        this.test = test;
+    }
+
+    public static TestEntity create(int id, String test)
+    {
+        return new TestEntity(id, test);
+    }
+}
