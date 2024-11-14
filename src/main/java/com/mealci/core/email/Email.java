58f1d9905f2cfbab68e22cd1c1d4a4cf9e376970
hugo <hugo.decuq.34@icloud.com>
2024-11-14 16:56:39 +0100

    Create Email value object

---
+++ src/main/java/com/mealci/core/email/Email.java
@@ -0,0 +1,31 @@
+package com.mealci.core.email;
+
+import com.mealci.core.base.ValueObject;
+import com.mealci.core.exceptions.CoreException;
+
+public class Email extends ValueObject {
+    private static final String EMAIL_PATTERN = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
+
+    public final String address;
+
+    private Email(String address) {
+        checkEmailValidity(address);
+
+        this.address = address;
+    }
+
+    public static Email create(String address) {
+        return new Email(address);
+    }
+
+    @Override
+    protected String toStringAttributes() {
+        return String.format("%s", address);
+    }
+
+    private static void checkEmailValidity(String email) {
+        if (email == null || email.isEmpty() || !email.matches(EMAIL_PATTERN)) {
+            throw new CoreException("Invalid email");
+        }
+    }
+}
