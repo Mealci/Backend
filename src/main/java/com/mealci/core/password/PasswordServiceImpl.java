e572637cfc6f1ee74c3a11127801234a6a786319
hugo <hugo.decuq.34@icloud.com>
2024-11-21 11:13:03 +0100

    Create password service

---
+++ src/main/java/com/mealci/core/password/PasswordServiceImpl.java
@@ -0,0 +1,26 @@
+package com.mealci.core.password;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.regex.Pattern;
+
+@Service
+public class PasswordServiceImpl implements PasswordService {
+    @Override
+    public boolean isPasswordValid(String password) {
+        return password != null
+                && password.length() >= 8
+                && Pattern.compile("[A-Z]").matcher(password).find()
+                && Pattern.compile("[a-z]").matcher(password).find()
+                && Pattern.compile("[0-9]").matcher(password).find()
+                && Pattern.compile("[!@#$%^&*(),.?\":{}|<>]").matcher(password).find();
+    }
+
+    @Override
+    public String hash(String password) {
+        var encoder = new BCryptPasswordEncoder();
+
+        return encoder.encode(password);
+    }
+}
