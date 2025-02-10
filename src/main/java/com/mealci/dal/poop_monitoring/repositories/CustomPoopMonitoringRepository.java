6b58150e83d86bd556ccc7091ed9f79d46a098f8
hugo <hugo.decuq.34@icloud.com>
2025-02-10 08:46:01 +0100

    Remove unused dependency

--- src/main/java/com/mealci/dal/poop_monitoring/repositories/CustomPoopMonitoringRepository.java
+++ src/main/java/com/mealci/dal/poop_monitoring/repositories/CustomPoopMonitoringRepository.java
@@ -3,7 +3,6 @@ package com.mealci.dal.poop_monitoring.repositories;
 import com.mealci.core.poop_monitoring.PoopMonitoring;
 
 import java.time.Instant;
-import java.util.Date;
 import java.util.List;
 
 public interface CustomPoopMonitoringRepository {
