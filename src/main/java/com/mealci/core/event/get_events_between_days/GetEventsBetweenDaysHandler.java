8bb1f87e7defb6413da436488db7213cbfa0663a
hugo <hugo.decuq.34@icloud.com>
2025-03-06 11:50:24 +0100

    Remove unused import

--- src/main/java/com/mealci/core/event/get_events_between_days/GetEventsBetweenDaysHandler.java
+++ src/main/java/com/mealci/core/event/get_events_between_days/GetEventsBetweenDaysHandler.java
@@ -14,7 +14,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 @Component
 public class GetEventsBetweenDaysHandler
